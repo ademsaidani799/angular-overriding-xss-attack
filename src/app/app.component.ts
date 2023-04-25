285a241b4899cf994ec0b9dbd8ca0bb404478877
adem saidani <ademsaidani799@gmail.com>
2023-04-26 00:48:13 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  searchKey: any;
+
+
+  search(searchTxt: any) {
+    this.searchKey = searchTxt.value
+  }
+}
